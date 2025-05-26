e2227285cfe0c2d6f517287ae342f98ca57143ab
VictorLirio <victordias_jobs@hotmail.com>
2025-05-26 19:22:39 -0300

    add module imports for Auth, Billing, Plans, Subscriptions, RateLimit, and Admin in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { BillingModule } from './billing/billing.module';
+import { PlansModule } from './plans/plans.module';
+import { SubscriptionsModule } from './subscriptions/subscriptions.module';
+import { RateLimitModule } from './rate-limit/rate-limit.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, BillingModule, PlansModule, SubscriptionsModule, RateLimitModule, AdminModule],
 })
 export class AppModule {}
