2275b088e34b35f932e0aad1446df59b55ed1112
VictorLirio <victordias_jobs@hotmail.com>
2025-05-30 18:16:43 -0300

    add validation schema for CreatePlanDto using Zod

--- src/plans/dto/create-plan.dto.ts
+++ src/plans/dto/create-plan.dto.ts
@@ -1,1 +1,12 @@
-export class CreatePlanDto {}
+import { z } from 'zod';
+
+export const CreatePlanSchema = z.object({
+  name: z.string().min(3).max(50),
+  description: z.string().min(10).max(500),
+  price: z.number().positive(),
+  billingCycle: z.number().int().positive(),
+  isActive: z.boolean().optional().default(true),
+  stripePriceId: z.string().optional(),
+});
+
+export type CreatePlanDto = z.infer<typeof CreatePlanSchema>;
