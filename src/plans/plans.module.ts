906714fb1fb75a6c453e7a694661dcfb32ac6e3f
VictorLirio <victordias_jobs@hotmail.com>
2025-06-04 18:03:37 -0300

    feat(plans): enhance PlansModule with TypeOrm, Billing, and EventEmitter integrations

--- src/plans/plans.module.ts
+++ src/plans/plans.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
-import { PlansService } from './plans.service';
-import { PlansController } from './plans.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Plan } from './entities/plan.entity';
+import { BillingModule } from '../billing/billing.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { PlansController } from './controller/plans.controller';
+import { PlansService } from './services/plans.service';
+import { PlansRepository } from './repositories/plans.repository';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Plan, PlansRepository]),
+    BillingModule,
+    EventEmitterModule.forRoot(),
+  ],
   controllers: [PlansController],
   providers: [PlansService],
+  exports: [PlansService],
 })
 export class PlansModule {}
